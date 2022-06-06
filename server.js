398314356b53fd8a56e9a0aa38499e49e4abd071
leerivera <leerivera80@gmail.com>
2022-06-06 18:14:41 -0400

    created listen event

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const app = express()
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
+
+app.listen(8000, ()=> {
+    console.log(`Server is doing it's thing thing.`)
+})
