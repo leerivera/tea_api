01cea0a1225bc6ad9cd85561476b711ca2dd6091
leerivera <leerivera80@gmail.com>
2022-06-06 18:33:41 -0400

    server working

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const { request } = require('express');
 const express = require('express')
 const app = express()
 const PORT = 8000;
+const tea = {
+    'green':{
+        'origin': 'China',
+        'waterTemp':'120',
+        'steepTime': '5',
+        'caffineLevel':'80mg',
+        'type': 'black',
+
+    }
+}
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (request,response) => {
+    response.json()
 })
 
 app.listen(PORT, ()=> {
